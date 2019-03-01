64fa31e6be88e8d6227c2d343b49eac00e600437
Cyril Rohr <cyril.rohr@gmail.com>
2019-03-01 16:59:46 +0100

    buildcurl integration

--- lib/language_pack/base.rb
+++ lib/language_pack/base.rb
@@ -28,7 +28,7 @@ class LanguagePack::Base
   # @param [String] the path of the cache dir this is nil during detect and release
   def initialize(build_path, cache_path = nil, layer_dir=nil)
     @build_path    = build_path
-    @stack         = ENV.fetch("STACK")
+    @stack         = ENV.fetch("STACK", "")
     @cache         = LanguagePack::Cache.new(cache_path)
     @metadata      = LanguagePack::Metadata.new(@cache)
     @bundler_cache = LanguagePack::BundlerCache.new(@cache, @stack)
