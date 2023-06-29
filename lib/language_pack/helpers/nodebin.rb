ea1ee29c931e0468381afd0e46880a4cbe44b46f
Cyril Rohr <cyril.rohr@gmail.com>
2023-06-29 18:32:39 +0000

    Use older nodejs on old distros

--- lib/language_pack/helpers/nodebin.rb
+++ lib/language_pack/helpers/nodebin.rb
@@ -2,10 +2,15 @@ require 'json'
 
 class LanguagePack::Helpers::Nodebin
   def self.hardcoded_node_lts
-    version = "16.18.1"
+    version = case ENV.fetch("TARGET")
+    when "ubuntu:18.04", "ubuntu:16.04", "el:7", "sles:12"
+      "16.18.1"
+    else
+      "18.16.1"
+    end
     {
       "number" => version,
-      "url"    => "https://heroku-nodebin.s3.us-east-1.amazonaws.com/node/release/linux-x64/node-v#{version}-linux-x64.tar.gz"
+      "url"    => "https://nodejs.org/dist/v#{version}/node-v#{version}-linux-x64.tar.gz"
     }
   end
 
